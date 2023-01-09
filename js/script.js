288a2cad09c7e51bdd60e2599b6146af29dc19d0
HEKEPOIU <decnovpk92805@gmail.com>
2023-01-09 21:36:06 +0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    
+try {
+    lottie.loadAnimation({
+        wrapper: A1,
+        animType: 'svg',
+        loop: true,
+        autoplay: true,
+        path: './json/part1.json'
+      });
+}
+catch (error) {}
+try {
+    lottie.loadAnimation({
+        wrapper: A2,
+        animType: 'svg',
+        loop: true,
+        autoplay: true,
+        path: './json/part2.json'
+      });
+}
+catch (error) {}
+try {
+    lottie.loadAnimation({
+        wrapper: A3,
+        animType: 'svg',
+        loop: true,
+        autoplay: true,
+        path: './json/part3.json'
+      });
+}
+catch (error) {}
+try {
+    lottie.loadAnimation({
+        wrapper: A4,
+        animType: 'svg',
+        loop: true,
+        autoplay: true,
+        path: './json/part4.json'
+      });
+}
+catch (error) {}
+try {
+    lottie.loadAnimation({
+        wrapper: A5,
+        animType: 'svg',
+        loop: true,
+        autoplay: true,
+        path: './json/part5.json'
+      });
+}
+  catch (error) { }
+
+})
